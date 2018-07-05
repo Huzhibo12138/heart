941bec0aaf6a64f841ece7f29ade5fd3ff789be3
wong <mvippp@163.com>
2018-07-05 11:49:28 +0800

    王文佩的完成版分支项目提交

---
+++ src/components/index/indexSection/firstSection/bags/bagList.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="bagList">
+      <div class="bagListmini" v-for="b in bList">
+          <div class="bagListminiImg">
+            <img :src="b.bagImg" alt="">
+          </div>
+          <p class="p1">
+            <span class="s1">{{b.news}}</span>
+            <span class="s2">{{b.title}}</span>
+          </p>
+          <p class="p2">
+            <span class="s3">￥{{b.newPrice}}</span>
+            <span class="s4">￥{{b.oldPrice}}</span>
+          </p>
+          <i class="iconfont icon-xin-copy"></i>
+      </div>
+
+    </div>
+</template>
+<script>
+    export default {
+        name: "bagList",
+      data(){
+        return{
+          bList:[
+            {bagImg:"./static/imgs/wxb1.jpg",news:'【9成新】',title:'Louis Vuitton',newPrice:4633,oldPrice:7688},
+            {bagImg:"./static/imgs/wxb2.jpg",news:'【8成新】',title:'ZENITH',newPrice:6888,oldPrice:9988},
+            {bagImg:"./static/imgs/wxb3.jpg",news:'【9成新】',title:'LONGINES',newPrice:2867,oldPrice:3888},
+            {bagImg:"./static/imgs/wxb4.jpg",news:'【8成新】',title:'Louis Vuitton',newPrice:3800,oldPrice:4988},
+            {bagImg:"./static/imgs/wxb5.jpg",news:'【9成新】',title:'FENDI',newPrice:2600,oldPrice:4288},
+            {bagImg:"./static/imgs/wxb6.jpg",news:'【9成新】',title:'Louis Vuitton',newPrice:3800,oldPrice:4988},
+            {bagImg:"./static/imgs/wxb7.jpg",news:'【7成新】',title:'MCM ',newPrice:6800,oldPrice:8888},
+            {bagImg:"./static/imgs/wxb8.jpg",news:'【8成新】',title:'COACH',newPrice:2500,oldPrice:8988},
+            {bagImg:"./static/imgs/wxb9.jpg",news:'【9成新】',title:'Moschino ',newPrice:1300,oldPrice:5908},
+            {bagImg:"./static/imgs/wxb10.jpg",news:'【9成新】',title:'Dior',newPrice:2380,oldPrice:4888},
+            {bagImg:"./static/imgs/wxb11.jpg",news:'【9成新】',title:'Cartier',newPrice:5368,oldPrice:9988},
+            {bagImg:"./static/imgs/wxb12.jpg",news:'【9成新】',title:'Hermès',newPrice:5800,oldPrice:18988},
+
+          ]
+        }
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .bagList{
+    span{
+      display: block;
+    }
+    width:100%;
+    padding:0.1rem;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    background:#f7f7f7;
+  }
+  .bagListmini{
+    width:49%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    padding-bottom:0.3rem;
+    margin-bottom: 0.05rem;
+    background:#ffffff;
+      .bagListminiImg img{
+        width:100%;
+        height:1.65rem;
+      }
+    .p1 {
+      display:flex;
+
+      padding:0.2rem 0 0;
+      .s1 {
+        font-size:0.12rem;
+        color:#252825;
+        line-height:0.24rem;
+        font-weight: 900;
+      }
+      .s2 {
+        font-weight:900;
+        font-size:0.1rem;
+        color:#535353;
+        line-height:0.24rem;
+        margin-left:4px;
+      }
+    }
+    .p2 {
+      display:flex;
+      .s3 {
+        font-size:0.16rem;
+        color:#0a0a0a;
+        line-height:0.26rem;
+      }
+      .s4 {
+        font-size:0.08rem;
+        color:#bab8ba;
+        line-height:0.26rem;
+        text-decoration: line-through;
+        margin-left:4px;
+      }
+    }
+    .iconfont{
+      font-size:0.2rem;
+      line-height:0.26rem;
+      position: absolute;
+      right:0.1rem;
+      bottom:0.1rem;
+
+    }
+
+
+  }
+
+
+</style>
---
+++ src/components/index/indexSection/firstSection/clothing/clothingList.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="clothingList">
+    <div class="clothingMini" v-for="wList in clothingL">
+        <div class="clothingMiniImg">
+          <img :src="wList.clothingImg" alt="">
+        </div>
+        <p class="p1">
+          <span class="s1">{{wList.news}}</span>
+          <span class="s2">{{wList.title}}</span>
+        </p>
+        <p class="p2">
+          <span class="s3">￥{{wList.newPrice}}</span>
+          <span class="s4">￥{{wList.oldPrice}}</span>
+        </p>
+      <i class="iconfont icon-xin-copy" @touchstart="touch"></i>
+      <div class="redheart" v-show="or_show">
+        <i class="iconfont icon-xin"></i>
+        <!--<img src="" alt="">-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "clothingList",
+    data(){
+      return{
+        or_show:false,
+        clothingL:[
+          {clothingImg:"./static/imgs/wvb1.jpg",news:'【9成新】',title:'J.Crew',newPrice:1288,oldPrice:5000},
+          {clothingImg:"./static/imgs/wvb2.jpg",news:'【8成新】',title:'Vince',newPrice:388,oldPrice:988},
+          {clothingImg:"./static/imgs/wvb3.jpg",news:'【全新】',title:'J.Crew',newPrice:867,oldPrice:1588},
+          {clothingImg:"./static/imgs/wvb4.jpg",news:'【全新】',title:'Under Armour',newPrice:567,oldPrice:1254},
+          {clothingImg:"./static/imgs/wvb5.jpg",news:'【全新】',title:'J.Crew',newPrice:1265,oldPrice:6500},
+          {clothingImg:"./static/imgs/wvb6.jpg",news:'【9成新】',title:'Vince',newPrice:1200,oldPrice:2300},
+          {clothingImg:"./static/imgs/wvb7.jpg",news:'【全新】',title:'Under Armour',newPrice:988,oldPrice:1100},
+          {clothingImg:"./static/imgs/wvb8.jpg",news:'【全新】',title:'J.Crew',newPrice:500,oldPrice:888},
+          {clothingImg:"./static/imgs/wvb9.jpg",news:'【9成新】',title:'Under Armour',newPrice:888,oldPrice:1500},
+          {clothingImg:"./static/imgs/wvb10.jpg",news:'【全新】',title:'Vince',newPrice:590,oldPrice:1300},
+
+        ]
+      }
+    },
+    methods:{
+      touch(ev){
+        var o = ev.target;
+        this.or_show = !this.or_show;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .clothingList{
+    span{
+      display: block;
+    }
+    width:100%;
+    padding:0.1rem;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    background:#f7f7f7;
+  }
+  .clothingMini{
+    width:49%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    padding-bottom:0.3rem;
+    margin-bottom: 0.05rem;
+    background:#ffffff;
+    .clothingMiniImg img{
+      width:100%;
+      height:1.65rem;
+    }
+    .p1 {
+      display:flex;
+
+      .s1 {
+        font-size:0.12rem;
+        color:#252825;
+        line-height:0.24rem;
+        font-weight:900;
+      }
+      .s2 {
+        font-size:0.1rem;
+        color:#454545;
+        font-weight:900;
+
+        line-height:0.24rem;
+        margin-left:4px;
+      }
+    }
+    .p2 {
+      display:flex;
+      .s3 {
+        font-size:0.16rem;
+        color:#0a0a0a;
+        line-height:0.26rem;
+      }
+      .s4 {
+        font-size:0.08rem;
+        color:#bab8ba;
+        line-height:0.26rem;
+        text-decoration: line-through;
+        margin-left:4px;
+      }
+    }
+    .iconfont{
+      font-size:0.2rem;
+      line-height:0.26rem;
+      position: absolute;
+      right:0.1rem;
+      bottom:0.1rem;
+
+    }
+    .redheart{
+      font-size:0.2rem;
+      line-height:0.26rem;
+      position: absolute;
+      right:0.01rem;
+      bottom:0.01rem;
+    }
+
+
+  }
+
+
+</style>
